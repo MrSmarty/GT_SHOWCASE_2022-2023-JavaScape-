e2e2128a6ee6ecafeb29c27f7da0de1ccbb9e477
Lincoln <MrSmartyMcFarty@gmail.com>
2022-10-25 22:31:49 -0500

    bug fixes

--- Pi Reciever/recieving.py
+++ Pi Reciever/recieving.py
@@ -35,7 +35,8 @@ async def printData():
             GPIO.output(pin, GPIO.HIGH)
         if state == False:
             GPIO.output(pin, GPIO.LOW)
-        print("Set pin # " + str(pin) + " to " + bool(state))
+        print("Set pin # " + str(pin) + " to " + str(state))
+
     elif data[:8] == "setupPin":
         pin = int(data[9:11])
         state = data[12:14]
